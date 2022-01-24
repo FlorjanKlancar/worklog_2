88ea16eeaa15f5ef521bad4a004523a7c2d56a69
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-01-24 10:30:08 +0100

    test

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,21 +1,22 @@
 import React from "react";
-import {getProviders, signIn as SignInFunc} from "next-auth/react";
+import { getProviders, signIn as SignInFunc } from "next-auth/react";
 
-function signin({providers}) {
+function signin({ providers }) {
   return (
     <div className="h-screen">
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name} className="pt-80">
-          <div>
-            <button
-              onClick={() => SignInFunc(provider.id, {callbackUrl: "/"})}
-              className="bg-blue-500 rounded-lg px-4 py-3 w-full text-white font-semibold"
-            >
-              Sign in with {provider.name}
-            </button>
+      {providers.length &&
+        Object.values(providers).map((provider) => (
+          <div key={provider.name} className="pt-80">
+            <div>
+              <button
+                onClick={() => SignInFunc(provider.id, { callbackUrl: "/" })}
+                className="bg-blue-500 rounded-lg px-4 py-3 w-full text-white font-semibold"
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
